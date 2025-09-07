bd7d342712cf2d65ea2586f6a8f2264a89183c47
keyasamanta124-hue <keyasamanta124@gmail.com>
2025-09-07 22:39:50 +0530

    Add files via upload

---
+++ Size of integer.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main() {
+    printf("size of int: %zu byte\n",sizeof(int));
+    printf("size of float: %zu byte\n", sizeof(float));
+    printf("size of double: %zu byte\n", sizeof(double));
+    printf("size of char: %zu byte\n", sizeof(double));
+    return 0;
+}
